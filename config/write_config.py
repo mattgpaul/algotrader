16c6f1df0d48c5170bbb027bcc2cbbc0ff6cf245
Matthew Paul <mattgpaul@gmail.com>
2022-12-15 15:39:40 -0700

    algotrader script created

--- config/write_config.py
+++ config/write_config.py
@@ -4,10 +4,10 @@ from configparser import ConfigParser
 config = ConfigParser()
 
 config.add_section('main')
-config.set('main', 'CLIENT_ID', 'FAKE_CLIENT_ID')
-config.set('main', 'REDIRECT_URI', 'FAKE_REDIRECT_URL')
-config.set('main', 'JSON_PATH', 'FAKE_PATH')
-config.set('main', 'ACCOUNT_NUMBER', 'FAKE_ACCT')
+config.set('main', 'CLIENT_ID', '')
+config.set('main', 'REDIRECT_URI', '')
+config.set('main', 'JSON_PATH', '')
+config.set('main', 'ACCOUNT_NUMBER', '')
 
-with open(file='config/config.ini', mode='w') as f:
+with open(file='config/config.ini', mode='w+') as f:
     config.write(f)
